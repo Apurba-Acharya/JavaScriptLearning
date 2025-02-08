ed56c2ac8135cb8bb27418d07d2469a8d8adc442
Apurba <apurba1907@gmail.com>
2025-02-08 23:31:56 +0530

    08/02/2025

---
+++ Step_1_1/3_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "apurba";
+
+console.log(typeof score); 
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+
+//"33" => can esily convert to Number
+//"33abc" => NaN = Not a number
+//true = 1; false = 0
+
+
+let isLoggedIn = "hitsh"; //if let isLoggedIn = "" : false
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=> true ; 0 => false
+//"" => false
+//"hitesh" => true
+
+
+let someNumber = 33;
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
