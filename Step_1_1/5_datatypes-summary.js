894405d508c1965b3d4c6f131ed43646b9116bcc
Apurba <apurba1907@gmail.com>
2025-02-09 15:49:05 +0530

    09/02

--- Step_1_1/5_datatypes-summary.js
+++ Step_1_1/5_datatypes-summary.js
@@ -1,7 +1,7 @@
 // Premitive:
 // 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-Example:
+//Example:
 const score = 100; // Number
 const scoreValue = 100.3; // Number
 
@@ -12,9 +12,29 @@ let userEmail; //undefined
 const id = Symbol('123');
 const anotherId = Symbol ('123')
 console.log(id == anotherId);
+const bignumber = 842025342632410632n;
 
 
 
 
 //Reference Type (Non Premitive): 
 //Array, Objects, Functions
+
+//Arrays:
+const heros = ["Shaktiman", "naagraj", "doga"];
+
+//Objects:
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+//Functions:
+const myFun = function(){
+    console.log("Hello world !");
+}
+
+console.log(typeof IsLoggedIn);
+console.log(typeof outsideTemp);
+
+// Link: https://262.ecma-international.org/5.1/#sec-11.4.3
