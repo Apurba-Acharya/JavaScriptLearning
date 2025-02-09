743cc87306712d3643a41cee82f65b4994c6d269
Apurba <apurba1907@gmail.com>
2025-02-09 15:35:39 +0530

    09/02

--- Step_1_1/3_conversionOperation.js
+++ Step_1_1/3_conversionOperation.js
@@ -25,3 +25,45 @@ let someNumber = 33;
 let stringNumber = String (someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+console.log ("********************** Operations *************************");
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2**2);
+//console.log(2%3);
+
+let str1 = "hello";
+let str2 = "JS";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "1");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+//PostFix:
+console.log(gameCounter++);
+//PreFix:
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
