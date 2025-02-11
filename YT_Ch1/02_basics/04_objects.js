e75547be5432700fd70cdaa604a2d671dccec66f
Apurba <apurba1907@gmail.com>
2025-02-12 00:57:07 +0530

    12/02

--- YT_Ch1/02_basics/04_objects.js
+++ YT_Ch1/02_basics/04_objects.js
@@ -3,7 +3,7 @@
 const tinderUser = {}; //non singeton object
 
 tinderUser.id = "123abc";
-tinderUser.nme = "Sammy";
+tinderUser.name = "Sammy";
 tinderUser.isLoggedIn = false
 
 //console.log(tinderUser);
@@ -22,11 +22,34 @@ const regularUser ={
 
 const obj1 = {1: "a", 2:"b"}
 const obj2 = {3: "a", 4:"b"}
+const obj3 = {5: "a", 6:"b"}
 
 //const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+const obj4 = Object.assign({}, obj1, obj2, obj3)
 
+const obj5 = {...obj1, ...obj2}
+//console.log(obj4);
+//console.log(obj5);
 
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+//console.log(users[1].email);
 
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
 
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
