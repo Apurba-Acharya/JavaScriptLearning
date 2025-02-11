6d53f8a4707b8377694065cdfa573425e7b30b2f
Apurba <apurba1907@gmail.com>
2025-02-12 00:40:00 +0530

    12/02

---
+++ YT_Ch1/02_basics/04_objects.js
@@ -0,0 +1,32 @@
+//const tinderUser = new Object() //singleton object
+//or:
+const tinderUser = {}; //non singeton object
+
+tinderUser.id = "123abc";
+tinderUser.nme = "Sammy";
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser ={
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Hitest",
+            lastname: "choudhary"
+        }
+    }
+}
+
+//console.log (regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "a", 4:"b"}
+
+//const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
+
+
+
