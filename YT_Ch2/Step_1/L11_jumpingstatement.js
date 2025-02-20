4247a9492d0c268ee75f2cd9305ca20ebcc81b7b
Apurba <apurba1907@gmail.com>
2025-02-20 18:56:28 +0530

    20/02

--- YT_Ch2/Step_1/L11_jumpingstatement.js
+++ YT_Ch2/Step_1/L11_jumpingstatement.js
@@ -8,10 +8,10 @@
 //     console.log(i);
 // }
 
-//Continue : Skipping the value in the loop
-for (let i=10;i>=1;i--){
-    if(i==5 || i==3 || i==9){
-        continue; // skpping the value
-    }
-    console.log(i);
-}
+// //Continue : Skipping the value in the loop
+// for (let i=10;i>=1;i--){
+//     if(i==5 || i==3 || i==9){
+//         continue; // skpping the value
+//     }
+//     console.log(i);
+// }
--- YT_Ch2/Step_1/L13_jsobjects.js
+++ YT_Ch2/Step_1/L13_jsobjects.js
@@ -1,41 +1,45 @@
-let person={
-    firstname: "John",
+//OBJECTS :
+
+let person={        // person = object
+    firstname: "John",  // Key : Property | Value : value of Property
     lastname: "Kenedy",
     age: 50,
     weight: 60
 }
-//Accessing Object Properties >>>
-//console.log(person["firstname"]);
-// Or,
-console.log(person.age);
+// //Accessing Object Properties >>>
+// console.log(person["firstname"]);
+// // Or,
+// console.log(person.age);
 
-console.log ("******** **********");
+// //Add new property to the existing person object :
+// //person ["height"] = 5.5;
+// //or,
+// person.height = 5.5;
+// console.log(person.height);
 
-//Add new property to the existing object:
-person ["height"] = 5.5;
-//or,
-//person.height = 5.5;
-console.log(person ["height"]); // or > console.log(person.height);
+// //Update existing property :
+// //person ["weight"]= 65;
+// // Or, 
+// person.weight = 65;
+// console.log(person.weight); 
 
-console.log ("******** **********");
+// //Remove the property from the object :
+// delete person.age;
+// console.log(person.age); //undefned
 
-//Update existing property
-person ["weight"]= 65;
-// Or, person.weight = 65;
-console.log(person ["weight"]); 
 
-console.log ("******** **********");
+//for-in loop for reading property statements :
 
-//Remove the property from the object
-delete person ["age"];
-console.log(person["age"]); //undefned
-
-console.log ("******** looping object **********");
-//for/in loop for reading property statements 
 for (let x in person){
-    // console.log(x); //prints only property names
-    // console.log(person[x]); ////prints only values of property
-    console.log(x+ ": "+person[x]);
+    console.log(x); //printing only property names
+}
+
+for (let y in person){
+    console.log(person[y]); //printing only values of property
+}
+
+for(let z in person){
+    console.log(z+ " : " +person[z]); // printing key value pair
 }
 
 
