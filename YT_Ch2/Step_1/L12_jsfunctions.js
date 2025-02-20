751a02da7e39f1fb09a5d1eaf732b7e092b3711f
Apurba <apurba1907@gmail.com>
2025-02-20 16:43:33 +0530

    20/02

--- YT_Ch2/Step_1/L12_jsfunctions.js
+++ YT_Ch2/Step_1/L12_jsfunctions.js
@@ -1,24 +1,26 @@
-//Example 1:
- 
+
+//FUNCTIONS :
+
+// //Example 1:
 // function add (a,b){
 //     console.log (a+b);
 //  }
 //  add (200,300);
 
-//Example 2:
+// //Example 2:
 // function add (a,b){
 //     return (a+b);
 // }
 // let result = add (10,20);
 // console.log(result)
 
-//Example 3:
+// //Example 3:
 // function greetins(){
 //     console.log("Good morning");
 // }
 // greetins();
 
-//Example 4:
+// //Example 4:
 // function greetins(){
 //     return("Good morning.....");
 // }
