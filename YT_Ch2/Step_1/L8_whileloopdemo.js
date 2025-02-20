5ea1bf338dac3bcd52a0abfc8c200bf74161cd0b
Apurba <apurba1907@gmail.com>
2025-02-20 11:07:22 +0530

    20/02

--- YT_Ch2/Step_1/L8_whileloopdemo.js
+++ YT_Ch2/Step_1/L8_whileloopdemo.js
@@ -1,20 +1,20 @@
-// 1 to 10
-/* let i = 1;
-while (i<=10){
-    console.log(i);
-    i++;
-} */
+// // Print 1 to 10 numbers :
+// let i = 1; //Initialization
+// while (i<=10){ //Condition
+//     console.log(i);
+//     i++; //i = i+1 //Increment / Decrement
+// }
 
-// even number between 1 to 10
-/* let i = 2;
-while (i<=10){
-    console.log(i);
-    i+=2; // i= i+2
-} */
+// // Print even number between 1 to 10 : 2, 4, 6, 8 ....
+// let i = 2;
+// while (i<=10){
+//     console.log(i);
+//     i+=2; // i= i+2
+// }
 
-// Decending order from 1 to 10
-let i = 10;
-while (i>0){
-    console.log(i);
-    i--; 
-}
+// // Decending order from 1 to 10 :
+// let i = 10;
+// while (i>=0){
+//     console.log(i);
+//     i--; 
+// }
--- YT_Ch2/Step_1/L9_dowhileloopdemo.js
+++ YT_Ch2/Step_1/L9_dowhileloopdemo.js
@@ -1,13 +1,15 @@
-//1 to 10
-/*
-let i = 10;
+// While loop : Without satisfying the conditions it will not execute the block
+/* Do-while : first time it will not verify any condition it will directly jump into the statements then verify the condition.
+              The statements will be executed at least 1 even the conditions is not satisfying. */
 
-do{
-    console.log(i);
-    i++;
-}while(i<=10); */
+// // Print : 1 to 10
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
 
-//10 to 1
+//Print reverse : 10 to 1
 let i=10;
 do{
     console.log(i);
