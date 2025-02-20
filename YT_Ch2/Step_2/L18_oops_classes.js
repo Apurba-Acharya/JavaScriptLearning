c06df5e84642c268e5acb8e953ea7cc2c080ac94
Apurba <apurba1907@gmail.com>
2025-02-20 23:18:44 +0530

    20/02

--- YT_Ch2/Step_2/L18_oops_classes.js
+++ YT_Ch2/Step_2/L18_oops_classes.js
@@ -1,38 +1,42 @@
 class Student { //Class = Attribute + Behaviours
 
-    // setDetails(sid,sname,grade){ //Attribute
-    //     //*This: is always representing the class
-    //     // this.sid = 101;
-    //     // this.sname = "John";
-    //     // this.grade = "A";
+    // Type 1 :
+    setDetails(sid,sname,grade){ // Attribute
+        //*This: is always representing the class
+        this.sid = sid;
+        this.sname = sname;
+        this.grade = grade;
+    }
 
-    //     this.sid = sid;
-    //     this.sname = sname;
-    //     this.grade = grade;
-    // }
+    display1(){ //Behaviours
+        console.log("setDetails display 1: ", this.sid,this.sname,this.grade);
+    }
 
+
+    // Type 2 :
     //CONSTRUCTOR: Constructor is only made for initializing the value. at the time of object creation constructor will be invoked.
-
     constructor(sid,sname,grade){
         this.sid = sid;
         this.sname = sname;
         this.grade = grade;
     }
-    display(){ //Behaviours
-        console.log(this.sid,this.sname,this.grade);
+    display2(){ //Behaviours
+        console.log("constructor display 2 : ", this.sid,this.sname,this.grade);
     }
+
 }
 
-// let stu = new Student();
-// stu.setDetails(101,"Dog","D");
-// stu1.display();
+// Type 1 :
+let stu = new Student(); // Object 1
+stu.setDetails(101,"Dog","D");
+stu.display1();
 
+//Type 2 :
+let stu1 = new Student(101,"Cow","E"); // Objects 2
+stu1.display2();
 
-let stu1 = new Student(101,"Cow","E"); // Objects 1
-stu1.display();
+let stu2 = new Student(101,"Bat","A++"); // Objects 3
+stu2.display2();
 
-let stu2 = new Student(101,"Bat","A++"); // Objects 2
-stu2.display();
-
-let stu3 = new Student(101,"Monkey","C"); // Objects 3
-stu3.display();
+let stu3 = new Student(101,"Monkey","C"); // Objects 4
+stu3.display2();
