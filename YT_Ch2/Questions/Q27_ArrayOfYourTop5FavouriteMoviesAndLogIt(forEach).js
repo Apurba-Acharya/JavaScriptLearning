7affacf89df8c9a5628f9a2efa4fd4950606d13a
Apurba <apurba1907@gmail.com>
2025-04-06 11:29:33 +0530

    06/04

---
+++ YT_Ch2/Questions/Q27_ArrayOfYourTop5FavouriteMoviesAndLogIt(forEach).js
@@ -0,0 +1,6 @@
+var arr = ["a", "b", "c", "d", "e", "f"];
+
+
+arr.forEach(function(value){
+    console.log(value)
+})
---
+++ YT_Ch2/Questions/Q29_AddNewElementsToTheStartOfAnArray.js
@@ -0,0 +1,4 @@
+var arr = [1, 2, 3, 4, 5];
+arr.unshift(0);
+arr.unshift(-1);
+console.log(arr);
